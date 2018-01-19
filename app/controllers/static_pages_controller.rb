352c9bcf15b2cad6b8892e57bcb66a691aea4ad5
Matthew <f0wlf4ng@gmail.com>
2018-01-19 05:40:39 -0700

    Updated CoffeeScript, added support for Windows. Static pages added.

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,13 @@
+class StaticPagesController < ApplicationController
+  def home
+  end
+
+  def accounts
+  end
+
+  def restaurants
+  end
+
+  def eat
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
+  get 'static_pages/home'
+
+  get 'static_pages/accounts'
+
+  get 'static_pages/restaurants'
+
+  get 'static_pages/eat'
+
+  root 'application#hello'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
