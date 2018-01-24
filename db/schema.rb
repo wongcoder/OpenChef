d493ba1e7f6810d151a255a70065588612d44266
Matthew <f0wlf4ng@gmail.com>
2018-01-23 18:01:43 -0700

    Temporary changes
    Testing!

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20180122031621) do
+
+  create_table "restaurants", force: :cascade do |t|
+    t.string "name"
+    t.string "location"
+    t.integer "rating"
+    t.string "contactPhone"
+    t.string "imageDirectory"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
