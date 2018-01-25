f72697200c54d0576052ae761aa17f917c9c265c
Matthew <f0wlf4ng@gmail.com>
2018-01-24 19:27:30 -0700

    Removed tmp folder, added test for restaurant.
    Temp folder holds server status.

--- config/routes.rb
+++ config/routes.rb
@@ -11,5 +11,6 @@ Rails.application.routes.draw do
 
   root 'application#hello'
 
+  resources :restaurants
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
