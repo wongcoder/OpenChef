fd8758da1c9b1da4924a4515a69f475dff4b7098
Maki 3 <f0wlf4ng@gmail.com>
2018-02-22 15:13:45 -0700

    moved functions to helper

--- app/helpers/static_pages_helper.rb
+++ app/helpers/static_pages_helper.rb
@@ -1,2 +1,8 @@
 module StaticPagesHelper
+  def welcomeLanguages()
+      languageArrays = ["Welcome.", "ようこそ", "환영"]
+      languageSelect = rand(3)
+      welcomeString = languageArrays[languageSelect]
+    return welcomeString
+  end
 end
