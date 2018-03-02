d8208e030f34978325fa5c534503311a93903afa
Maki 3 <f0wlf4ng@gmail.com>
2018-03-01 17:58:47 -0700

    adding nav

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,15 +4,7 @@ module ApplicationHelper
     if page_title.empty?
       base_title
     else
-      page_title + "|" + base_title
+      page_title + "|" + "OpenChef"
     end
   end
-
-  def welcomeLanguages()
-      languageArrays = ["Welcome.", "ようこそ", "환영"]
-      languageSelect = rand(3)
-      welcomeString = languageArrays[languageSelect]
-    return welcomeString
-  end
-  
 end
